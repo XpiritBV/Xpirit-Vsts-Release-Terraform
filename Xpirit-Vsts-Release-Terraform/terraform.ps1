e3e5c9f6be574b5763ae576825cf442583ba4387
Ahmed IG <ahmedsayedig@gmail.com>
2019-06-06 14:45:57 +1000

    Fix terraform task to work with the new TF 0.12.1

--- Xpirit-Vsts-Release-Terraform/terraform.ps1
+++ Xpirit-Vsts-Release-Terraform/terraform.ps1
@@ -2,15 +2,12 @@ Trace-VstsEnteringInvocation $MyInvocation
 
 #region ImportFunctions
 $functionFolders = @('Public')
-ForEach ($folder in $functionFolders)
-{
+ForEach ($folder in $functionFolders) {
     $folderPath = Join-Path -Path $PSScriptRoot -ChildPath $folder
-    If (Test-Path -Path $folderPath)
-    {
+    If (Test-Path -Path $folderPath) {
         Write-Verbose -Message "Importing from $folder"
         $functions = Get-ChildItem -Path $folderPath -Filter '*.ps1'
-        ForEach ($function in $functions)
-        {
+        ForEach ($function in $functions) {
             Write-Verbose -Message "  Importing $($function.BaseName)"
             . $($function.FullName)
         }
@@ -19,16 +16,15 @@ ForEach ($folder in $functionFolders)
 #endregion
 
 #region FunctionDefinitions
-function Install-Terraform
-{
+function Install-Terraform {
     $version = Get-VstsInput -Name Version
 
     # Need to force using more up-to-date encryption protocols; Hashicorp is on-point deprecating broken ones.
     [System.Net.ServicePointManager]::SecurityProtocol = `
-                [System.Net.SecurityProtocolType]::Tls11 -bor 
-                [System.Net.SecurityProtocolType]::Tls12 -bor `
-                [System.Net.SecurityProtocolType]::Tls -bor `
-                [System.Net.SecurityProtocolType]::Ssl3
+        [System.Net.SecurityProtocolType]::Tls11 -bor 
+    [System.Net.SecurityProtocolType]::Tls12 -bor `
+        [System.Net.SecurityProtocolType]::Tls -bor `
+        [System.Net.SecurityProtocolType]::Ssl3
 
     $terraformbaseurl = "https://releases.hashicorp.com/terraform/"
     $path = "c:\terraform-download"
@@ -38,40 +34,33 @@ function Install-Terraform
     $webpage = (Invoke-WebRequest $terraformbaseurl -UseBasicParsing).Content
 
     $versions = $webpage -split "`n" | Select-String -pattern $regex -AllMatches | % { $_.Matches | % { $_.Groups[1].Value } }
-    if ($version -eq "latest")
-    {
+    if ($version -eq "latest") {
         $version = $versions[0]
     }
-    else
-    {
-        if (-not $versions.Contains($version))
-        {   
+    else {
+        if (-not $versions.Contains($version)) {   
             throw [System.Exception] "$version not found."
         }
     }
 
     $tempfile = [System.IO.Path]::GetTempFileName()
-    $source = "https://releases.hashicorp.com/terraform/"+$version+"/terraform_"+$version+"_windows_amd64.zip"
+    $source = "https://releases.hashicorp.com/terraform/" + $version + "/terraform_" + $version + "_windows_amd64.zip"
 
     Invoke-WebRequest $source -UseBasicParsing -OutFile $tempfile
 
-    if (-not (test-path $path))
-    {
+    if (-not (test-path $path)) {
         mkdir $path
     }
 
     $P = [Environment]::GetEnvironmentVariable("PATH")
-    if($P -notlike "*"+$path+"*")
-    {
+    if ($P -notlike "*" + $path + "*") {
         [Environment]::SetEnvironmentVariable("PATH", "$P;$path")
     }
 
     Add-Type -AssemblyName System.IO.Compression.FileSystem
-    function Unzip
-    {
+    function Unzip {
         param([string]$zipfile, [string]$outpath)
-        if (test-path $outpath)
-        {
+        if (test-path $outpath) {
             del "$outpath\*.*"
         }
         [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
@@ -83,21 +72,17 @@ function Install-Terraform
     terraform --version
 }
 
-function Get-BackendConfig
-{
+function Get-BackendConfig {
     $tfFiles = Get-ChildItem -Include "*.tf" -File -Recurse
-    Foreach ($file in $tfFiles)
-    {
+    Foreach ($file in $tfFiles) {
         $fileContents = [System.IO.File]::ReadAllText($file)
-        if ($fileContents -Match "backend `"azurerm`"")
-        {
+        if ($fileContents -Match "backend `"azurerm`"") {
             $startIndex = $fileContents.IndexOf("backend `"azurerm`"")
             $startIndex = $fileContents.IndexOf('{', $startIndex) + 1            
             $endIndex = $fileContents.IndexOf('}', $startIndex)            
             $keyValues = $fileContents.Substring($startIndex, $endIndex - $startIndex)
             $backendConfig = ConvertFrom-StringData -StringData $keyValues     
-            foreach ($key in $($backendConfig.Keys))
-            {
+            foreach ($key in $($backendConfig.Keys)) {
                 $backendConfig.Item($key) = $backendConfig.Item($key).Trim("`"", " ")                
             }            
             return $backendConfig
@@ -105,8 +90,7 @@ function Get-BackendConfig
     }    
 }
 
-function Initialize-Terraform
-{       
+function Initialize-Terraform {       
     $connectedServiceName = Get-VstsInput -Name ConnectedServiceNameARM -Require
     $endpoint = Get-VstsEndpoint -Name $connectedServiceName -Require
 
@@ -117,7 +101,7 @@ function Initialize-Terraform
     $remoteStateArguments = "-backend-config=`"arm_subscription_id=$($endpoint.Data.subscriptionId)`" -backend-config=`"arm_tenant_id=$($endpoint.Auth.Parameters.TenantId)`" -backend-config=`"arm_client_id=$($endpoint.Auth.Parameters.ServicePrincipalId)`" -backend-config=`"arm_client_secret=$($endpoint.Auth.Parameters.ServicePrincipalKey)`""
 
     $specifyStorageAccount = Get-VstsInput -name SpecifyStorageAccount -Require -AsBool
-    if ($specifyStorageAccount){
+    if ($specifyStorageAccount) {
         $resourceGroupName = Get-VstsInput -Name StorageAccountResourceGroup -Require
         $storageAccountName = Get-VstsInput -Name StorageAccountRM -Require
         $storageContainerName = Get-VstsInput -Name StorageContainerName -Require  
@@ -132,33 +116,33 @@ function Initialize-Terraform
         $extraArguments = "";
     }
 
-    if (-not ([string]::IsNullOrEmpty($additionalArguments)))
-    {
+    if (-not ([string]::IsNullOrEmpty($additionalArguments))) {
         $arguments = $remoteStateArguments + " $($additionalArguments.Trim()) $($extraArguments.Trim())"
-    } else {
+    }
+    else {
         $arguments = $remoteStateArguments + " $($extraArguments.Trim())"
     }
        
     Invoke-VstsTool -FileName terraform -arguments "init $arguments"
 
-    if ($LASTEXITCODE)
-    {
+    if ($LASTEXITCODE) {
         $E = $Error[0]
         Write-Host "##vso[task.logissue type=error;] Terraform init failed to execute. Error: $E" 
         Write-Host "##vso[task.complete result=Failed]"
     }
 }
 
-function Invoke-Terraform
-{
+function Invoke-Terraform {
     $arguments = (Get-VstsInput -Name Arguments -Require) -split '\s+'
 
     $defaultArgs = "-input=false -no-color " + (Get-VstsInput -Name PlanPath)
+    if ($arguments.Trim() -like "validate*") {
+        $defaultArgs = "-no-color " + (Get-VstsInput -Name PlanPath)
+    }
     
     Invoke-VstsTool -FileName terraform -arguments "$($arguments.Trim()) $($defaultArgs.TrimEnd())"
 
-    if ($LASTEXITCODE)
-    {
+    if ($LASTEXITCODE) {
         $E = $Error[0]
         Write-Host "##vso[task.logissue type=error;] Terraform failed to execute. Error: $E" 
         Write-Host "##vso[task.complete result=Failed]"
@@ -181,19 +165,20 @@ $useAzureSub = Get-VstsInput -Name UseAzureSub -Require -AsBool
 $manageTerraformState = Get-VstsInput -Name ManageState -Require -AsBool
 $specifyStorageAccount = Get-VstsInput -name SpecifyStorageAccount -Require -AsBool
 
-if ($useAzureSub){
+if ($useAzureSub) {
     Import-EnvVars
 }
 
-if ($useAzureSub -and $manageTerraformState){
+if ($useAzureSub -and $manageTerraformState) {
     # Initialize Azure.
     Import-Module $PSScriptRoot\ps_modules\VstsAzureHelpers_
     Initialize-Azure
-    if ($specifyStorageAccount){
+    if ($specifyStorageAccount) {
         $resourceGroupName = Get-VstsInput -Name StorageAccountResourceGroup -Require
         $storageAccountName = Get-VstsInput -Name StorageAccountRM -Require
         $storageContainerName = Get-VstsInput -Name StorageContainerName -Require 
-    } else {
+    }
+    else {
         $backendConfig = Get-BackendConfig
         $resourceGroupName = $backendConfig.resource_group_name
         $storageAccountName = $backendConfig.storage_account_name
@@ -202,11 +187,11 @@ if ($useAzureSub -and $manageTerraformState){
     Ensure-AzStorageContainerExists -ResourceGroupName $resourceGroupName -StorageAccountName $storageAccountName -StorageContainer $storageContainerName    
 }
 
-if ($installTerraform){
+if ($installTerraform) {
     Install-Terraform
 }
 
-if ($useAzureSub -and $manageTerraformState){
+if ($useAzureSub -and $manageTerraformState) {
     Initialize-Terraform
 }
 
